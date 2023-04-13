262945fb33680d6438b575f50050b7e5424b12e2
KuntaWasonga <wkunta@rocketmail.com>
2023-04-14 00:50:30 +0300

    Added union annotation

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Defines a function that sums up a list."""
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
     """Function that returns the sum of a mixed list."""
     total = 0
     for i in range(len(mxd_lst)):
